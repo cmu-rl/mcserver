9eb0fb0e0b8ef7dcf67bd9e72be03483199ffe3a
Cayden <ccodel@andrew.cmu.edu>
2018-07-02 11:09:55 -0400

    Removed apt install

--- setup.sh
+++ setup.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-apt-get updata
-apt install gcc -y
-apt install openjdk-8-jre-headless -y
-source /home/ubuntu/.bashrc
-
-ssh-keyscan github.com >> /home/ubuntu/.ssh/known_hosts
-ssh-keyscan git.code.sf.net >> /home/ubuntu/.ssh/known_hosts
-
-git clone https://github.com/ccodel/mcserver.git
-git clone https://github.com/cmu-rl/HerobrainePlugin.git
-git clone git://git.code.sf.net/p/mcrcon/code mcrcon-code
-
-chown -R ubuntu:ubuntu /home/ubuntu/mcserver/
-chown -R ubuntu:ubuntu /home/ubuntu/mcrcon-code/
-chown -R ubuntu:ubuntu /home/ubuntu/HerobrainePlugin/
-
 cd mcrcon-code
 gcc mcrcon.c -o mcrcon
 cp /home/ubuntu/mcrcon-code/mcrcon /home/ubuntu/
