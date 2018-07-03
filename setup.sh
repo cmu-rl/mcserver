b04d5beb4d03544a298713b74c7e9dcc6253ea34
Cayden <ccodel@andrew.cmu.edu>
2018-07-03 12:18:19 -0400

    Changed plugins a bit

--- setup.sh
+++ setup.sh
@@ -3,13 +3,5 @@ cd /home/ubuntu/mcrcon-code
 gcc mcrcon.c -o mcrcon
 cp /home/ubuntu/mcrcon-code/mcrcon /home/ubuntu/
 
-cd /home/ubuntu/HerobrainePlugin/src/main
-
-jar cf HerobrainePlugin.jar java/io resources/config.yml resources/plugin.yml resources/worlds.yml
-mv HerobrainePlugin.jar /home/ubuntu/mcserver/plugins/
-
-cd /home/ubuntu
-rm -rf HerobrainePlugin
-
 cd /home/ubuntu/mcserver
 /usr/bin/java -jar craftbukkit-1.12.2.jar
