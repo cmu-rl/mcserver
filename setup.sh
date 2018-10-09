0ca9d42d3b23afce1cc8122950709f5690990aa3
Cayden <ccodel@andrew.cmu.edu>
2018-10-09 14:49:09 -0400

    Added new lobby, paperclip jar

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ gcc mcrcon.c -o mcrcon
 cp /home/ubuntu/mcrcon-code/mcrcon /home/ubuntu/
 
 cd /home/ubuntu/mcserver
-/usr/bin/java -Xms3G -Xmx3G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar craftbukkit-1.12.2.jar
+#/usr/bin/java -Xms13G -Xmx13G -jar craftbukkit-1.12.2.jar
+/usr/bin/java -Xms13G -Xmx13G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar paperclip.jar
