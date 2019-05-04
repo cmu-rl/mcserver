1414a18ef15426282f0f37f4db68ca854ad19c78
Nat Quayle Nelson <natquaylenelson@gmail.com>
2019-05-04 17:48:57 -0600

    Lobby with rules

---
+++ git-add-lobby.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# git add the parts of the lobby world data that contain the lobby itself
+
+git add lobby/region/r.0.0.mca
+git add lobby/region/r.-1.0.mca
+git add lobby/region/r.-1.-1.mca
+git add lobby/region/r.0.-1.mca
